1f3983847c3dbf6ac2e9a114440784ab0e925a12
ayumitanaka <tanakayumi13@gmail.com>
2021-05-31 22:27:57 +0000

    added Procfile

--- server/index.js
+++ server/index.js
@@ -16,6 +16,10 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
